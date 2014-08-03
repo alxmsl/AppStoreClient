914b054289d035120885b487768118fce529bcef
Alexey Maslov <alexey.y.maslov@gmail.com>
2014-08-04 00:40:39 +0400

    added WTFPL license.

--- examples/subscription.php
+++ examples/subscription.php
@@ -1,5 +1,11 @@
 <?php
 /**
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details.
+ *
  * App Store In-App purchase receipt verification
  * @author alxmsl
  * @date 2/16/13
--- source/AbstractClient.php
+++ source/AbstractClient.php
@@ -1,7 +1,14 @@
 <?php
+/*
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details.
+ */
 
 namespace alxmsl\AppStore;
-use Network\Http\Request;
+use alxmsl\Network\Http\Request;
 
 /**
  * Abstract AppStore client class
